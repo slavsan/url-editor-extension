5c54c6edd717e1ee231216e607c786307b1c2313
Slavcho Slavchev <slavsan.mail@gmail.com>
2018-03-14 19:46:55 +0200

    Update dependencies

--- src/popup/components/FieldsList.jsx
+++ src/popup/components/FieldsList.jsx
@@ -18,65 +18,67 @@ const mapStateToProps = state => {
 const FieldsList = ({ dispatch, url, fields, visibleFields }) => {
   return (
     <div>
-      <Table>
-        <tbody>
-          {visibleFields.length === 0 && (
-            <div className="no-results">No fields match this search</div>
-          )}
-          {visibleFields.map(field => (
-            <tr key={field.id}>
-              <td>
-                <ControlLabel>{field.name}</ControlLabel>
-              </td>
-              <td>
-                {typeof field.value === 'object' ? (
-                  <div>
-                    {field.value.map(f => (
-                      <FormGroup key={f.key}>
-                        <ControlLabel>{f.key}</ControlLabel>
-                        <FormControl
-                          type="text"
-                          value={f.value}
-                          spellCheck={false}
-                          onKeyUp={(e) => {
-                            if (e.keyCode === 13) {
-                              utils.changeURL(utils.buildURL(url, fields))
-                            }
-                          }}
-                          onChange={e => {
-                            dispatch(updateNestedField({
-                              id: field.id,
-                              key: f.key,
-                              value: e.target.value
-                            }))
-                          }}
-                        />
-                      </FormGroup>
-                    ))}
-                  </div>
-                ) : (
-                  <FormControl
-                    type="text"
-                    value={field.value}
-                    spellCheck={false}
-                    onKeyUp={(e) => {
-                      if (e.keyCode === 13) {
-                        utils.changeURL(utils.buildURL(url, fields))
-                      }
-                    }}
-                    onChange={e => {
-                      dispatch(updateField({
-                        id: field.id,
-                        value: e.target.value
-                      }))
-                    }}
-                  />
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      {visibleFields.length === 0 && (
+        <div className="no-results">No fields match this search</div>
+      )}
+      {visibleFields.length > 0 && (
+        <Table>
+          <tbody>
+            {visibleFields.map(field => (
+              <tr key={field.id}>
+                <td>
+                  <ControlLabel>{field.name}</ControlLabel>
+                </td>
+                <td>
+                  {typeof field.value === 'object' ? (
+                    <div>
+                      {field.value.map(f => (
+                        <FormGroup key={f.key}>
+                          <ControlLabel>{f.key}</ControlLabel>
+                          <FormControl
+                            type="text"
+                            value={f.value}
+                            spellCheck={false}
+                            onKeyUp={(e) => {
+                              if (e.keyCode === 13) {
+                                utils.changeURL(utils.buildURL(url, fields))
+                              }
+                            }}
+                            onChange={e => {
+                              dispatch(updateNestedField({
+                                id: field.id,
+                                key: f.key,
+                                value: e.target.value
+                              }))
+                            }}
+                          />
+                        </FormGroup>
+                      ))}
+                    </div>
+                  ) : (
+                    <FormControl
+                      type="text"
+                      value={field.value}
+                      spellCheck={false}
+                      onKeyUp={(e) => {
+                        if (e.keyCode === 13) {
+                          utils.changeURL(utils.buildURL(url, fields))
+                        }
+                      }}
+                      onChange={e => {
+                        dispatch(updateField({
+                          id: field.id,
+                          value: e.target.value
+                        }))
+                      }}
+                    />
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
     </div>
   )
 }
